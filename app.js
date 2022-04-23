0f0cbe636b6aa01124e2397c7f1f10a6850ae44c
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-04-23 14:37:44 -0500

    mongo atlas

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://admin:root@cluster0.kiyqs.mongodb.net/todolistDB");
 
 const itemsSchema = new mongoose.Schema ({
   name: String
